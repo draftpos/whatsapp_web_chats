fb577784d3c5fe5b6f9ec529f18c518d9241ab9d
makonia20 <makonia20@gmail.com>
2026-09-08 14:45:03 +0200

    Use dynamic background colors for contact avatars based on name hash

--- static/src/js/chats.js
+++ static/src/js/chats.js
@@ -239,6 +239,21 @@ export class WhatsAppChatsAction extends Component {
         }
     }
 
+    getAvatarColor(name) {
+        if (!name) return '#dfe5e7';
+        const colors = [
+            '#e57373', '#f06292', '#ba68c8', '#9575cd', '#7986cb', 
+            '#64b5f6', '#4fc3f7', '#4dd0e1', '#4db6ac', '#81c784', 
+            '#aed581', '#ff8a65', '#d4e157', '#ffd54f', '#ffb74d', '#a1887f', '#90a4ae'
+        ];
+        let hash = 0;
+        for (let i = 0; i < name.length; i++) {
+            hash = name.charCodeAt(i) + ((hash << 5) - hash);
+        }
+        hash = Math.abs(hash);
+        return colors[hash % colors.length];
+    }
+
     async loadProducts() {
         try {
             const results = await this.orm.searchRead(
