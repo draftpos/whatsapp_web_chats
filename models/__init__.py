e82b6b760d5e9e5d1fdffb11fcfaf60ba23af9b7
makonia20 <makonia20@gmail.com>
2026-09-14 12:53:35 +0200

    Fix model import order for followup rules

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 from . import res_config_settings
 from . import wa_chat_tag
+from . import whatsapp_followup_rule
+from . import whatsapp_scheduled_message
 from . import whatsapp_account
 from . import product_template
 from . import whatsapp_product
@@ -10,5 +12,3 @@ from . import res_users
 from . import discuss_channel
 from . import whatsapp_quick_reply
 from . import ir_ui_menu
-from . import whatsapp_followup_rule
-from . import whatsapp_scheduled_message
